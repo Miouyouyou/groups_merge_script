14f6bcb69d2863c820a4bf62567827c3c3607190
Miouyouyou <myy@miouyouyou.fr>
2020-12-05 10:23:16 +0100

    Let's avoid SSH for now
    This really pose some issues with autobuilders

--- godot_v_sekai_update.sh
+++ godot_v_sekai_update.sh
@@ -24,9 +24,8 @@ git remote add godotengine https://github.com/godotengine/godot
 git remote set-url --push godotengine https://example.com/
 git fetch godotengine
 git remote rm v-sekai-godot || true
-git remote add v-sekai-godot git@github.com:v-sekai/godot.git
-# Need permission to push
-#git remote set-url --push v-sekai-godot https://example.com/
+git remote add v-sekai-godot https://github.com/v-sekai/godot.git
+git remote set-url --push v-sekai-godot https://example.com/
 git fetch v-sekai-godot
 git remote rm TwistedTwigleg || true
 git remote add TwistedTwigleg https://github.com/TwistedTwigleg/godot.git
@@ -53,7 +52,7 @@ python3 ./thirdparty/git-assembler -av
 git checkout groups -f
 export MERGE_DATE=$(date --iso=sec --utc)
 export MERGE_TAG=$(echo groups.$MERGE_DATE | tr ':' ' ' | tr -d ' \t\n\r')
-git merge -s ours remotes/v-sekai-godot/groups -m "Commited at $MERGE_DATE."
+git merge -s recursive -X ours remotes/v-sekai-godot/groups -m "Commited at $MERGE_DATE."
 git tag -a $MERGE_TAG -m "Commited at $MERGE_DATE."
 git push v-sekai-godot $MERGE_TAG
 git push v-sekai-godot groups
